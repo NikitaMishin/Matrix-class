ac13731a46e4284c7156edfca10f69d7d4894606
NikitaMishin <nikitamishin97@mail.com>
2017-02-04 08:42:23 +0300

    added SimpleTransformation for matrix(header)

---
+++ simpleTransformation.c
@@ -0,0 +1,14 @@
+#include "simpleTransformation.h"
+
+ //#define
+//а
+
+//void div_mul_simpleTransformation(matrix **A,int scalar,char orient);
+//void add_simpleTransformation(matrix **A,int to;int what; char orient )
+//void sub_simpleTransformation(matrix **A,int to;int what; char orient )
+//void mul_simpleTransformation(matrix **A,int scalar)
+//void div_simpleTransformation(matrix **A,int scalar)
+void swap_simpleTransformation(matrix *A,int what,int with;char orient)
+{
+
+}
---
+++ simpleTransformation.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+
+void add_simpleTransformation(matrix **A,int to;int what; char orient );
+void sub_simpleTransformation(matrix **A,int to;int what; char orient );
+void mul_simpleTransformation(matrix **A,int scalar);
+void div_simpleTransformation(matrix **A,int scalar);
+void swap_simpleTransformation(matrix **A,int what,int with;char orient );
